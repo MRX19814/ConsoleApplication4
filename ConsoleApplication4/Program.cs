c3d3265488d677b8c3c4a2ac9ae85197004dd17c
MRX <kurganovss666@gmail.com>
2023-04-24 17:31:33 +0300

    Task 4.3.17

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -113,7 +113,7 @@ namespace ConsoleApplication4
             Console.WriteLine(num);
         }*/ // Task 4.3.15
 
-        static void Main(string[] args) 
+        /*static void Main(string[] args) 
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
             int temp = 0;
@@ -126,6 +126,29 @@ namespace ConsoleApplication4
                 }
             }
             Console.WriteLine(temp);
+        }*/ // Task 4.3.16
+
+        static void Main(string[] args) 
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+            int temp;
+            for ( int i = 0; i <= arr.GetUpperBound(0); i++ ) 
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++ ) 
+                    for (int k = j+1; k <= arr.GetUpperBound(1); k++ )
+                        if (arr[i,j] > arr[i,k] )
+                    {
+                       temp = arr[i,k];
+                       arr[i,k] = arr[i,j];
+                       arr[i,j] = temp;
+                    }
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
